0554dbbf2f00181c382f70f5b67ae54a28c06667
+ <jacqueline.trapp18@gmail.com>
2025-01-27 10:36:56 -0500

    removing unsused code

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ from datetime import datetime
 # from flask_migrate import Migrate
 from flask import request, jsonify, session, make_response, render_template
 from flask_restful import  Resource
-# import os
-
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-# DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 @app.before_request
 def check_if_logged_in():
